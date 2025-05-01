1884c224c49a5e283d73d3f7385a8a39fe7b0688
Walery Demidko <walerydz@gmail.com>
2025-05-01 14:28:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 class Person:
-    # write your code here
-    pass
+    people = dict()
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.update({self.name: self})
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    persons = list()
+    for human in people:
+        persons.append(Person(human["name"], human["age"]))
+
+        persons_index = persons.index(persons[-1])
+        human_spouse_key = list(human.keys())[2]
+        if human[human_spouse_key]:
+            human_spouse_value = human[human_spouse_key]
+            setattr(
+                persons[persons_index],
+                human_spouse_key,
+                human_spouse_value
+            )
+
+    for person in persons:
+        if hasattr(person, "wife"):
+            person.wife = person.people.get(person.wife)
+        elif hasattr(person, "husband"):
+            person.husband = person.people.get(person.husband)
+
+    return persons
